7c8d1002de1f5760957ac1359058bafedd553a67
Kaserp Kraegpoeth <kraegpoeth@gmail.com>
2017-01-10 07:17:39 +0100

    removed passing of variables to massage template

--- imports/ui/messages.js
+++ imports/ui/messages.js
@@ -5,8 +5,10 @@ import { moment } from 'meteor/momentjs:moment';
 
 import { Messages } from '../api/messages.js';
 
-import './messages.html';
 import './message.html';
+
+import './messages.html';
+
 
 
 Template.messages.helpers({
@@ -15,15 +17,6 @@ Template.messages.helpers({
   },
 });
 
-/*
-Template.registerHelper("timestampToTime", function(timestamp){
-  var date = new Date(timestamp);
-  var hours = date.getHours();
-  var minutes = "0" + date.getMinutes();
-  var seconds = "0" + date.getSeconds();
-  return hours + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);
-});
-*/
 
 Template.registerHelper("timeFormatted", function(createdAt){
   return moment(createdAt).format('HH:mm:ss')
